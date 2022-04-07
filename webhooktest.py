d9fee5ae4113dc43157eead75ec938200fa181fd
Will Nash <willnash@wills-mbp.misc.iastate.edu>
2022-04-07 10:24:27 -0500

    Removed Test file

--- webhooktest.py
+++
@@ -1,5 +0,0 @@
-from discord_webhook import DiscordWebhook
-webhook = DiscordWebhook(
-            url='https://discord.com/api/webhooks/961624378628522014/yrmtVU96SaAYdJumn3SoFuJQ15ze9BjvsbQS3MTQyFXr7PfAk1oqO5bAzMtNFWfJtohi',
-            content='Webhook Message'
-            )
